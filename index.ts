f4af14335ee366a0337667565a44b93770af2d73
codewithzubi <za8089006@gmail.com>
2024-05-23 01:44:25 -0700

    Word_Counter
    This is my Word Counter Project

---
+++ index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.cyanBright("\n \t\t ZUBAIR AHMED - WORD COUNTER"));
+console.log("=".repeat(70));
+
+let answers = await inquirer.prompt({
+  type: "input",
+  name: "sentence",
+  message: "ENTER THE SENTENCE FOR COUNT THE WORD",
+});
+
+let words = answers.sentence.trim().split(" ");
+
+console.log("=".repeat(70));
+console.log(chalk.bold("Sentence Words:"));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(70));
+
